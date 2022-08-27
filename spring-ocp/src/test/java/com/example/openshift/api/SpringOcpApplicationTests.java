cb519e279a8163ff2d323e0e2b991e97fe405659
sougata <bagchi.sougata@gmail.com>
2022-08-28 00:42:45 +0530

    Initial commit

---
+++ spring-ocp/src/test/java/com/example/openshift/api/SpringOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openshift.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
